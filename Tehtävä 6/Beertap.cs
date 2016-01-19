669f4e0f26ad2a17b25e5563694015dcf518c68b
SnakeRule <jerevaltanen@gmail.com>
2016-01-19 18:01:57 +0200

    Updated

---
+++ Tehtävä 6/Beertap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_6
+{
+    class Beertap
+    {
+        public int Smallbeers;
+        public int Mediumbeers;
+        public int Largebeers;
+        public int Small_moneyspent;
+        public int Medium_moneyspent;
+        public int Large_moneyspent;
+
+        public void Pour_smallbeer()
+        {
+            Console.WriteLine("You pour a tasty small beer");
+            Smallbeers++;
+            Small_moneyspent = Small_moneyspent + 2;
+        }
+
+        public void Pour_mediumbeer()
+        {
+            Console.WriteLine("You pour an excellent medium beer");
+            Mediumbeers++;
+            Medium_moneyspent = Medium_moneyspent + 3;
+        }
+
+        public void Pour_largebeer()
+        {
+            Console.WriteLine("You pour an amazing large beer");
+            Largebeers++;
+            Large_moneyspent = Large_moneyspent + 5;
+        }
+
+        public void How_drunk()
+        {
+            Console.WriteLine("You have {0} promilles of alcohol in your blood." ,(Smallbeers * 0.1 + Mediumbeers * 0.3 + Largebeers * 0.5));
+        }
+
+        public void Money_spent()
+        {
+            Console.WriteLine( "You have spent {0} euros." ,Small_moneyspent + Medium_moneyspent + Large_moneyspent);
+        }
+    }
+}
---
+++ Tehtävä 6/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beertap beertap = new Beertap();
+
+            int input;
+            Console.WriteLine("Welcome to the beer pouring/drinking simulator!");
+            do
+            {
+                Console.WriteLine("1. Pour small beer");
+                Console.WriteLine("2. Pour medium beer");
+                Console.WriteLine("3. Pour Large beer");
+                Console.WriteLine("4. Check how drunk you are");
+                Console.WriteLine("5. Check how much money you have spent");
+                Console.WriteLine("6. Go home");
+                input = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                switch(input)
+                {
+                    case 1:
+                        {
+                            beertap.Pour_smallbeer();
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            beertap.Pour_mediumbeer();
+                            break;
+                        }
+                    case 3:
+                        {
+                            beertap.Pour_largebeer();
+                            break;
+                        }
+                    case 4:
+                        {
+                            beertap.How_drunk();
+                            break;
+                        }
+                    case 5:
+                        {
+                            beertap.Money_spent();
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("Loser");
+                            break;
+                        }
+                }
+
+            } while (input != 6);
+
+            Console.ReadLine();
+        }
+    }
+}
