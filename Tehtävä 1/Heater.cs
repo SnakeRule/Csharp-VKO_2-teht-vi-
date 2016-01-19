5e569ba1a71a8ddd600c2980725f6c7a6a968aa8
Jere Valtanen <jerevaltanen@msn.com>
2016-01-19 10:47:10 +0200

    Eh

---
+++ Tehtävä 1/Heater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_1
+{
+    class Heater
+    {
+        public bool Is_On { get; set; }
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+
+        // method prints heater data
+        public void PrintData()
+        {
+            Console.WriteLine("Heater: ");
+            Console.WriteLine("- Power {0},  Temperature : {1}, Humidity: {2} ", Is_On, Temperature, Humidity);
+
+        }
+
+        // method returns heater data
+        public override string ToString()
+        {
+            return "Temperature : " + Temperature + ", Humidity : " + Humidity;
+        }
+
+    }
+}
---
+++ Tehtävä 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new Heater object
+            Heater heater = new Heater();
+            heater.Is_On = true;
+            heater.Temperature = 100;
+            heater.Humidity = 95;
+
+            heater.PrintData();
+
+            Console.WriteLine(heater.ToString());
+        }
+    }
+}
