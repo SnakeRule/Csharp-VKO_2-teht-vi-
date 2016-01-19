6d4bb5eb5b921a344175fc263f136355eba24efd
Jere Valtanen <jerevaltanen@msn.com>
2016-01-19 12:02:50 +0200

    Updated

---
+++ Tehtävä 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a new vehicle object
+            Vehicle vehicle = new Vehicle();
+
+            //assigning data to vehicle object
+            vehicle.Name = "Audi";
+            vehicle.Speed = 300;
+            vehicle.Tyres = 4;
+
+            vehicle.PrintData();
+
+            Console.WriteLine("And using another printing method");
+
+            Console.WriteLine(vehicle.ToString());
+
+
+   
+        }
+    }
+}
---
+++ Tehtävä 4/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿/*
+* Copyright (C) Jere-Joonas Valtanen
+*
+* This file is part of JAMK object oriented programming course
+*
+* Created: 19.1.2016
+*Author: Jere-Joonas Valtanen
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_4
+{
+    class Vehicle
+    {
+        // 
+
+        public string Name;
+        public int Speed;
+        public int Tyres;
+
+        //method for printing out vehicle data
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle name : {0} ", Name);
+            Console.WriteLine("Speed : {0}", Speed);
+            Console.WriteLine("Tyres: {0}", Tyres);
+        }
+
+        public override string ToString()
+        {
+            return "Vehicle name : " + Name + ", Speed : " + Speed + ", Tyres : " + Tyres;
+        }
+    }
+}
